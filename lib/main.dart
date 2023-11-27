a767384d8582300a3d49536a9e0c0a66e8f678d2
ferrykurniawan28 <147892622+ferrykurniawan28@users.noreply.github.com>
2023-11-27 08:58:10 +0700

    second_update

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Timer AEO 2024',
       theme: ThemeData.dark(),
       // home: const MainScreenMobile(),
       initialRoute: '/',
